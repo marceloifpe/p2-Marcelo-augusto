6c4c2e081e9458e2e68e0362477aa49b295d5b09
marceloifpe <44452440+marceloifpe@users.noreply.github.com>
2022-12-20 15:19:17 -0300

    Classe Personagem-Funcional

--- Mini-Projeto3/Personagem.java
+++ Mini-Projeto3/Personagem.java
@@ -1,33 +1,32 @@
 public class Personagem {
-     String nome;
-     int energia;
+    String nome;
+    int energia;
 
-     Personagem(String nome, int energia)
-     {
-                this.nome = nome;
-                this.energia = 100;
-     }
+    Personagem(String nome, int energia)
+    {
+               this.nome = nome;
+               this.energia = 100;
+    }
 
-     void alterarEnergia(int alteracao){
-        this.energia = this.energia +alteracao;
+    void alterarEnergia(int alteracao){
+       this.energia = this.energia +alteracao;
 
-        if(alteracao > 0)
-        {
-            System.out.println("Este movimento, impactou no cavaleiro "+ this.nome + " restaurando "+ alteracao +" Pontos de Energia. ");
-        }
-        else {
-            System.out.println("Este movimento, impactou no cavaleiro "+ this.nome + " consumindo "+ alteracao +" Pontos de Energia. ");
-        }
+       if(alteracao > 0)
+       {
+           System.out.println("Este movimento, impactou no cavaleiro "+ this.nome + " restaurando "+ alteracao +" Pontos de Energia. ");
+       }
+       else {
+           System.out.println("Este movimento, impactou no cavaleiro "+ this.nome + " consumindo "+ alteracao +" Pontos de Energia. ");
+       }
 
-        if(this.energia > 100)
-        {
-            this.energia = 100;
-        }
-        else if (this.energia < 0)
-        {
-            this.energia = 0;
-            System.out.println(this.nome +" foi derrotado.");
-        }
-     }
+       if(this.energia > 100)
+       {
+           this.energia = 100;
+       }
+       else if (this.energia < 0)
+       {
+           this.energia = 0;
+           System.out.println(this.nome +" foi derrotado.");
+       }
+    }
 }
-     
